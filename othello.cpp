f7f042d2f5392b763c82cd124801e41a83457073
mohammad_mahdi_haeri <mahdihaerim@gmail.com>
2022-12-06 10:35:43 +0330

    add game_control class and some function for checking a block is placeable

--- othello.cpp
+++ othello.cpp
@@ -54,6 +54,55 @@ public:
 private:
 };
 
+class Game_control {
+public:
+  Game_control(Map *_map) {
+    map = _map;
+  }
+
+  bool is_placeable(int i, int j) {
+    for (int p = -1; p <= 1; p++) {
+      for (int q = -1; q <= 1; q++) {
+        if (go(i, j, p, q, 0) > 0) {
+          return true;
+        }
+      }
+    }
+    return false; 
+  }
+
+  int go(int i, int j, int p, int q, int k) {
+    if (!is_there_in_map(i + p, j + q)) {
+      return -100;
+    } else if (map->map[i][j] == BLANK) {
+      return -100;
+    } else if (map->map[i + p][j + q] == player_turn()) {
+      return k;
+    } else {
+      go(i + p, j + q, p, q, k + 1);
+    }
+  }
+
+  bool is_there_in_map(int i, int j) {
+    if (i >= 0 && i < SIZE && j >= 0 && j < SIZE) {
+      return true;
+    }
+    return false;
+  }
+
+  char player_turn() {
+    return turn % 2 == 0 ? PLAYER_1 : PLAYER_2;
+  }
+
+  int player_score_1 = 0;
+  int player_score_2 = 0;
+  int turn = 0;
+  Map *map;
+private:
+};
+
+
+
 int main(int argc, char const *argv[]) {
   Map map;
 
